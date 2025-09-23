5f342c1a101c42b33dc52ff54eed74f767b77632
Leevi81 <145373321+Leevi81@users.noreply.github.com>
2025-09-23 19:17:09 +0300

    chore: return the whole job table

--- bridge/esx/server.lua
+++ bridge/esx/server.lua
@@ -6,7 +6,6 @@ function bridge.getPlayerJob(source)
     if GetResourceState('es_extended') ~= 'started' then lib.print.error('Selected framework is esx, but es_extended is not started') return end
     local Player = ESX.GetPlayerFromId(source)
     local job = Player.getJob()
-    local playerJob = job.name
 
     return playerJob
 end
