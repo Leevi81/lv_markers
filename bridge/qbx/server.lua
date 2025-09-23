f387b610c470c856b56a70c1272428a0fcd97ad6
Leevi81 <145373321+Leevi81@users.noreply.github.com>
2025-09-23 19:17:56 +0300

    chore: return the whole job table

--- bridge/qbx/server.lua
+++ bridge/qbx/server.lua
@@ -3,7 +3,7 @@ local bridge = {}
 function bridge.getPlayerJob(source)
     if GetResourceState('qbx_core') ~= 'started' then lib.print.error('Selected framework is qbx, but qbx_core is not started') return end
     local Player = exports.qbx_core:GetPlayer(source)
-    local playerJob = Player.PlayerData.job.name
+    local playerJob = Player.PlayerData.job
 
     return playerJob
 end
