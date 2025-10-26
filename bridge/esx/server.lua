d621569fd6604e0839bb9be763818356bb54aae6
Leevi81 <145373321+Leevi81@users.noreply.github.com>
2025-10-26 18:23:16 +0200

    fix: return invalid parameter

--- bridge/esx/server.lua
+++ bridge/esx/server.lua
@@ -5,10 +5,11 @@ local ESX = exports['es_extended']:getSharedObject()
 function bridge.getPlayerJob(source)
     if GetResourceState('es_extended') ~= 'started' then lib.print.error('Selected framework is esx, but es_extended is not started') return end
     local Player = ESX.GetPlayerFromId(source)
-    local job = Player.getJob()
+    local playerJob = Player.getJob()
 
     return playerJob
 end
 
 
 return bridge
+
